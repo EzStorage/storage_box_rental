eec7f689a548ab2db013e1696c80b4578111c890
minhnbh <computer.hoangminh@gmail.com>
2025-05-29 16:03:04 +0700

    ui: custom button

---
+++ src/components/button/my-button.tsx
@@ -0,0 +1,15 @@
+import { ButtonProps } from "@mui/material";
+import { StyledButton } from "./my-button.styles";
+
+export interface MyButtonProps extends ButtonProps {
+  variantType: "login" | "primary";
+}
+
+const MyButton: React.FC<MyButtonProps> = ({ variantType = "primary", ...props }) => {
+  return (
+    <StyledButton variantType={variantType} {...props}>
+      {props.children}
+    </StyledButton>
+  );
+};
+export default MyButton;
