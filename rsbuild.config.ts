490fe1682f94f3c97d1c5277e0ac1004c1c7f9d8
minhnbh <computer.hoangminh@gmail.com>
2025-06-02 23:06:52 +0700

    update rsbuild config

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -3,4 +3,16 @@ import { pluginReact } from '@rsbuild/plugin-react';
 
 export default defineConfig({
   plugins: [pluginReact()],
+  html: {
+    template: "./public/index.html",
+  },
+  output: {
+    distPath: {
+      root: "build",
+    },
+    sourceMap: {
+      css: false,
+      js: "source-map",
+    }
+  }
 });
