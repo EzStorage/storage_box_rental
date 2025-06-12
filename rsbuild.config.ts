6a2a66f726bc4ac741b069a6d764adc885925294
minhnbh <computer.hoangminh@gmail.com>
2025-06-12 22:39:02 +0700

    update rs build config

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -1,18 +1,18 @@
-import { defineConfig } from '@rsbuild/core';
-import { pluginReact } from '@rsbuild/plugin-react';
+import { defineConfig } from "@rsbuild/core";
+import { pluginReact } from "@rsbuild/plugin-react";
 
 export default defineConfig({
-  plugins: [pluginReact()],
-  // html: {
-  //   template: "./public/index.html",
-  // },
-  // output: {
-  //   distPath: {
-  //     root: "build",
-  //   },
-  //   sourceMap: {
-  //     css: false,
-  //     js: "source-map",
-  //   }
-  // }
+    plugins: [pluginReact()],
+    html: {
+        template: "./public/index.html",
+    },
+    output: {
+        distPath: {
+            root: "build",
+        },
+        sourceMap: {
+            css: false,
+            js: "source-map",
+        },
+    },
 });
