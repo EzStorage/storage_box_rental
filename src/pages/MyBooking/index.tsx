93e5cee7f91ce2f4c9e85ab893b4b022ab9be62c
Akash-Verma-learns <akashvermancr@gmail.com>
2025-07-21 14:50:58 +0530

    Resolvetomock

--- src/pages/MyBooking/index.tsx
+++ src/pages/MyBooking/index.tsx
@@ -9,11 +9,41 @@ import { useEffect } from "react";
 import { fakeRequest } from "src/services/mockHttp";
 import { CircularProgress } from "@mui/material";
 import { Box } from "@mui/material";
+import { mockBookings } from "src/constants/MockData";
+import { useMyBookingCommit } from "./context";
+
 export const MyBookingContent = () => {
     const navigate = useNavigate();
     const handleNewStorageClick = () => {
         navigate("/booking");
     };
+    const [isLoading, setIsLoading] = useState(true);
+    const commit = useMyBookingCommit();
+
+    useEffect(() => {
+        const loadFakeData = async () => {
+            setIsLoading(true);
+            const result = await fakeRequest({ resolve: () => mockBookings });
+            commit({
+                bookings: result,
+            });
+            setIsLoading(false);
+        };
+        loadFakeData();
+    }, []);
+
+    if (isLoading) {
+        return (
+            <Box
+                height="calc(100vh - 72px)"
+                display="flex"
+                justifyContent="center"
+                alignItems="center"
+            >
+                <CircularProgress />
+            </Box>
+        );
+    }
     return (
         <Container>
             <Wrapper>
@@ -34,15 +64,7 @@ export const MyBooking = () => {
     const { type } = useParams<{ type: string }>();
     const navigate = useNavigate();
     const [, setSearchParams] = useSearchParams();
-    const [isLoading, setIsLoading] = useState(true);
-    useEffect(() => {
-        const loadFakeData = async () => {
-            setIsLoading(true);
-            await fakeRequest({ resolve: () => ({}) });
-            setIsLoading(false);
-        };
-        loadFakeData();
-    }, []);
+
     useEffect(() => {
         if (!type) {
             if (!Object.values(BookingTabs).includes(type)) {
@@ -52,18 +74,7 @@ export const MyBooking = () => {
             navigate("/my-bookings/upcoming", { replace: true });
         }
     }, [type, setSearchParams, navigate]);
-    if (!type || isLoading) {
-        return (
-            <Box
-                height="calc(100vh - 72px)"
-                display="flex"
-                justifyContent="center"
-                alignItems="center"
-            >
-                <CircularProgress />
-            </Box>
-        );
-    }
+
     if (!type) return null;
 
     return (
